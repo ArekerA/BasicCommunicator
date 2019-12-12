776aef01062af4113f1e799d1d3427b966d2f47a
ArekerA <Arek.ludwikowski@hotmail.com>
2019-12-12 22:41:45 +0100

    First setp to authorize by google auth

--- auth.go
+++ auth.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/stretchr/gomniauth"
+)
 
 type authHandler struct {
 	next http.Handler
@@ -21,4 +27,32 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := ""
+	provider := ""
+	if len(segs) >= 4 {
+		action = segs[2]
+		provider = segs[3]
+	}
+	switch action {
+	case "login":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Błąd podczas próby pobrania dostawcy %s %s", provider, err), http.StatusBadRequest)
+			return
+		}
+		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Błąd wywołania GetBeginAuthURL %s %s", provider, err), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", loginUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "Akcja autoryzacyjna %s nie jest obsługiwana", action)
+	}
+
 }
--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHnadler struct {
@@ -25,9 +30,17 @@ func (t *templateHnadler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "Adres aplikacji internetowej")
 	flag.Parse()
+	gomniauth.SetSecurityKey("123456789")
+	gomniauth.WithProviders(
+		github.New("id", "key", "http://localhost:8080/auth/callback/github"),
+		google.New("461495886139-h9jrtuo5eipstjnf84uruf1q0u3cfkrt.apps.googleusercontent.com", "QlF6B9ENdZ5CNO_y_e3pZmUo", "http://localhost:8080/auth/callback/google"),
+		facebook.New("id", "key", "http://localhost:8080/auth/callback/facebook"),
+	)
 	r := newRoom()
 	//r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHnadler{filename: "chat.html"}))
+	http.Handle("/login", &templateHnadler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	go r.run()
 	if err := http.ListenAndServe(*addr, nil); err != nil {
